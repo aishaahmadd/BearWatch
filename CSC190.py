c4436e5295174d35d3374d9482664bd988961449
Aisha Ahmad <ahmadaisha368@gmail.com>
2025-02-06 15:50:14 -0500

    Creating stock graph and search
    Created search bar and stock graph framework

--- CSC190.py
+++ CSC190.py
@@ -1,22 +1,67 @@
 import datetime
 import yfinance as yf
 import time
+import matplotlib.pyplot as plt
+import pandas as pd
+from dash import dash, dcc, html, Input, Output, callback
+import plotly.graph_objs as go
+from dash.dependencies import Input, Output
+
+# Initialize the Dash app
+app = dash.Dash(__name__)
+
+# Define the layout of the dashboard
+app.layout = html.Div([
+   html.H1("Custom Trading Dashboard"),  # Title of the dashboard
+   dcc.Input(
+       id="search",
+            type="text",
+            placeholder="Search".format("text"),
+            debounce=True
+   ),
+   dcc.Graph(id='price-chart')  # Placeholder for the price chart
+])
+
+# Fetch historical market data
+def fetch_data(ticker):
+   stock = yf.Ticker(ticker)
+   hist = stock.history(period="1mo",interval="1h")  # Retrieve data for the past year
+   return hist
+
+def create_price_chart(df):
+   fig = go.Figure()
+   fig.add_trace(go.Scatter(x=df.index, y=df['Close'], mode='lines', name='Close Price'))
+   fig.update_layout(title='Stock Price Over Time', xaxis_title='Date', yaxis_title='Price')
+   return fig
+
+@app.callback(
+   Output('price-chart', 'figure'),
+   [Input("search", "value")]
+)
+def update_price_chart(ticker):
+   df = pd.DataFrame(fetch_data(ticker))
+   fig = create_price_chart(df)
+   return fig
+
+# Run the app
+if __name__ == '__main__':
+   app.run_server(debug=True)
+
+
 
 '''
 Importing stock data (Open, High, Low, Close) for userticker every 10 seconds.
 Datetime from output yyyy-mm-dd hh:mm:00 DOES NOT SUPPORT SECONDS.
 '''
-
-def fetch_data(userticker: str) -> None:
+'''
+def fetch_day_data(userticker: str) -> None:
     ticker = yf.Ticker(userticker)
     data = ticker.history(period="1d", interval="1m")
-    print(data.tail(1))  # Display the most recent data point
+    #print(data.tail(1))  # Display the most recent data point
+    print(data)
 
-# used to get current time, can be used for display
-now = datetime.datetime.now()
-print(now)
-
-if __name__ == "__main__":
+def display_day_stock_data(ticker: str) -> None:
     while True:
-        fetch_data("AAPL")
+        data = fetch_day_data(ticker)
         time.sleep(10)  # Wait for 10 sec before fetching the data again
+'''
